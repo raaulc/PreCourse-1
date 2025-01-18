3c87a8826bb391e72a331d7d94ea5c00ab466128
Rahul Rathod <65287267+raaulc@users.noreply.github.com>
2025-01-18 22:47:05 +0000

    added homework #18Jan

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,80 @@
-
 class Node:
-    def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
-    def push(self, data):
-        
+        self.top = None
+
+    def push(self, val):
+        node = Node(val)
+        # Insert node at the start of the linked list
+        node.next = self.top
+        self.top = node
+
     def pop(self):
-        
+        if self.top is None:
+            print("Stack is empty")
+            return None
+        top_element = self.top
+        self.top = top_element.next
+        return top_element.data
+
+    def size(self):
+        stack_size = 0
+        current_node = self.top
+        while current_node:
+            stack_size += 1
+            current_node = current_node.next
+        return stack_size
+    
+    def print_stack(self):
+        current_node = self.top
+        while current_node:
+            print('||',current_node.data,'||')
+            current_node = current_node.next
+        print('-------------')
+
+    def get_top(self):
+        if self.top is None:
+            return None
+        return self.top.data
+    
+    def is_empty(self):
+        return self.top is None
+
+
+
 a_stack = Stack()
-while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
+a_stack.push(1)
+a_stack.push(2)
+a_stack.push(3)
+a_stack.push(4)
+a_stack.print_stack()
+print('Size - ', a_stack.size())
+print("pooped element - ", a_stack.pop())
+a_stack.print_stack()
+a_stack.get_top()
+print('is stack empty? ', a_stack.is_empty())
+print('Size - ', a_stack.size())
+
+# while True:
+#     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+#     print('push <value>')
+#     print('pop')
+#     print('quit')
+#     do = input('What would you like to do? ').split()
+#     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+#     operation = do[0].strip().lower()
+#     if operation == 'push':
+#         a_stack.push(int(do[1]))
+#     elif operation == 'pop':
+#         popped = a_stack.pop()
+#         if popped is None:
+#             print('Stack is empty.')
+#         else:
+#             print('Popped value: ', int(popped))
+#     elif operation == 'quit':
+#         break
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +19,68 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+
+        while current:
+            if current.data == key:
+                if previous is None:
+                    self.head = current.next
+                else:
+                    previous.next = current.next
+                return True  # Element removed
+            previous = current
+            current = current.next
+        return False  
+
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+
+    sll.append(1)
+    sll.append(2)
+    sll.append(3)
+
+    found_node = sll.find(2)
+    if found_node:
+        print(f"Found: {found_node.data}")
+    else:
+        print("Not found.")
+
+    result = sll.remove(2)
+    if result:
+        print("Element removed.")
+    else:
+        print("Element not found.")
+
+    found_node = sll.find(2)
+    if found_node:
+        print(f"Found: {found_node.data}")
+    else:
+        print("Not found.")
